4e591f15ccd65d98655fbca681e1f9c5030a0946
flameking1000 <elijahalstonjr@gmail.com>
2024-04-10 06:38:59 -0400

    set up more of fovus
    added compnents

---
+++ fovus-app/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import textinput 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      
+        <a
+          className="App-link"
+          href="https://docs.google.com/document/d/1UJUpYPP8u-9AFvzWergmr8gNweNV6v6YSMBZOJpX718/edit"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fovus Coding Project Instructions
+        </a>
+      </header>
+
+
+      <div className="Fovus-app">
+
+
+<h2> APP</h2> </div>
+
+
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ fovus-app/src/components/textinput/textinput/textinput.js
@@ -0,0 +1,16 @@
+import textStyles from './textStyles'
+
+ export default function textinput(){
+
+return(
+
+
+    <form type="text"> Text input: </form>
+
+
+
+
+);
+ 
+
+}
